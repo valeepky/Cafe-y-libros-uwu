738e49c0d97a2754426bd0cd38451129ff9f7b77
pio-pio-17 <fioturinsanchez@crackthecode.la>
2025-12-17 16:49:58 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-import Header from "./componentes/header";
+import Header from "./componentes/Header";
 import Hero from "./componentes/Hero";
 import Footer from "./componentes/Footer";
 
